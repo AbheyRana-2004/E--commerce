d8684aa36285d9258b0436553d9253c8f011c085
Abhey Rana <abheyrana3114@gmail.com>
2025-01-17 18:02:16 +0530

    responsive products name

--- src/components/productscompo/singleproduct.jsx
+++ src/components/productscompo/singleproduct.jsx
@@ -7,19 +7,23 @@ const singleproduct = ({ value }) => {
   return (
     <>
         <NavLink to={`/product/${value.id}`}>
-      <div className="p-4 w-[180px] border border-black rounded-xl " key={value.id}>
-        <img
-          src={value.image}
-          className="w-full h-[180px] object-contain"
-          alt=""
-        />
-        <h2 className="whitespace-nowrap overflow-hidden text-ellipsis">
-          {value.title}
-        </h2>
-        <h3>Price : ${value.price}</h3>
-         
-       
-      </div>
+        <div
+  className="p-4 w-[180px] sm:w-[300px] md:w-[500px] lg:w-[700px] xl:w-[900px] border border-black rounded-xl"
+  key={value.id}
+>
+  <img
+    src={value.image}
+    className="w-full h-[180px] sm:h-[240px] md:h-[300px] lg:h-[350px] xl:h-[400px] object-contain"
+    alt=""
+  />
+  <h2 className="whitespace-nowrap overflow-hidden text-ellipsis text-sm sm:text-base md:text-lg lg:text-xl">
+    {value.title}
+  </h2>
+  <h3 className="text-sm sm:text-base md:text-lg lg:text-xl">
+    Price : ${value.price}
+  </h3>
+</div>
+
           </NavLink>
     </>
   );
